9c09439ff388d9500da9fad9d3628a7ff961f15d
jatcorominas <jatcorominas@gmail.com>
2019-11-23 15:35:52 -0500

    Invoke asynchronous CustomerService CRUD methods

--- CustomersMongoDb/Controllers/CustomersController.cs
+++ CustomersMongoDb/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using CustomersMongoDb.Models;
 using CustomersMongoDb.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -19,85 +20,124 @@ namespace CustomersMongoDb.Controllers
 
         // GET: api/Customers
         [HttpGet]
-        public ActionResult<List<Customer>> Get() =>
-            _customerService.Get();
-
-        // GET: api/Customers/5
-        [HttpGet("{id:length(24)}", Name = "GetCustomerById")]
-        public ActionResult<Customer> GetById([FromRoute] string id)
+        public async Task<ActionResult> GetCustomers()
         {
-            var customer = _customerService.GetById(id);
-
-            if (customer == null)
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                return BadRequest(ModelState);
             }
 
-            return customer;
-        }
-
-        // GET: api/Customers/age/30
-        [HttpGet("age/{age}")]
-        public ActionResult<List<Customer>> GetByAge([FromRoute] int age)
-        {
-            var customers = _customerService.GetByAge(age);
+            IEnumerable<Customer> customers = await _customerService.Get();
 
             if (customers == null)
             {
                 return NotFound();
             }
 
-            return customers;
+            return Ok(customers);
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id:length(24)}", Name = "GetCustomerById")]
+        public async Task<ActionResult> GetCustomerById([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = await _customerService.GetById(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // GET: api/Customers/age/30
+        [HttpGet("age/{age}")]
+        public async Task<ActionResult> GetCustomerByAge([FromRoute] int age)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IEnumerable<Customer> customers = await _customerService.GetByAge(age);
+
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customers);
         }
 
         // POST: api/Customers/create
         [HttpPost("create")]
-        public ActionResult<Customer> Create([FromBody] Customer customer)
+        public async Task<IActionResult> PostCustomer([FromBody] Customer customer)
         {
-            _customerService.Create(customer);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-            return CreatedAtRoute("GetCustomerById", new { id = customer.Id.ToString() }, customer);
+            await _customerService.Create(customer);
+
+            return CreatedAtAction("GetCustomer", new { Id = customer.Id.ToString() }, customer);
         }
 
         // PUT: api/Customers/5
         [HttpPut("{id:length(24)}")]
-        public ActionResult<Customer> Update([FromRoute] string id, [FromBody] Customer customerIn)
+        public async Task<IActionResult> PutCustomer([FromRoute] string id, [FromBody] Customer customerIn)
         {
-            var customer = _customerService.GetById(id);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            var customer = await _customerService.GetById(id);
             if (customer == null)
             {
                 return NotFound();
             }
 
-            _customerService.Update(id, customerIn);
+            await _customerService.Update(id, customerIn);
 
-            return customerIn;
+            return Ok( customerIn);
+
         }
 
         // DELETE: api/Customers/5
         [HttpDelete("{id:length(24)}")]
-        public IActionResult Delete([FromRoute] string id)
+        public async Task<IActionResult> DeleteCustomer([FromRoute] string id)
         {
-            var customer = _customerService.GetById(id);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = await _customerService.GetById(id);
 
             if (customer == null)
             {
                 return NotFound();
             }
 
-            _customerService.Remove(customer.Id);
+            await _customerService.Remove(customer.Id);
 
-            return NoContent();
+            return Ok("Successfully deleted customer");
         }
 
         // DELETE: api/Customers/delete
         [HttpDelete("delete")]
-        public IActionResult DeleteAll()
+        public async Task<IActionResult> DeleteAll()
         {
-            _customerService.RemoveAll();
+            await _customerService.RemoveAll();
 
-            return NoContent();
+            return Ok("Deleted All Customers");
         }
     }
 
